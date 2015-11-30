b8fea72aad6516e62a2d30d458c5f16f7ee2f4a4
deanrobertcook <deanrobertcook@gmail.com>
2015-12-01 06:04:58 +0800

    Remove update logic from DBHelper
    This is uneccessary now that the backup feature exists

--- app/src/main/java/org/theronin/budgettracker/data/BudgetDbHelper.java
+++ app/src/main/java/org/theronin/budgettracker/data/BudgetDbHelper.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import org.theronin.budgettracker.BuildConfig;
 import org.theronin.budgettracker.data.BudgetContract.CategoriesTable;
+import org.theronin.budgettracker.data.BudgetContract.CategoriesView;
 import org.theronin.budgettracker.data.BudgetContract.EntriesTable;
 
 public class BudgetDbHelper extends SQLiteOpenHelper {
@@ -22,53 +22,25 @@ public class BudgetDbHelper extends SQLiteOpenHelper {
         createTables(sqLiteDatabase);
     }
 
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        dropTables(sqLiteDatabase);
+        createTables(sqLiteDatabase);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        dropTables(sqLiteDatabase);
+        createTables(sqLiteDatabase);
+    }
+
     public static void createTables(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CategoriesTable.SQL_CREATE_CATEGORIES_TABLE);
         sqLiteDatabase.execSQL(EntriesTable.SQL_CREATE_ENTRIES_TABLE);
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        switch (oldVersion) {
-            case 1:
-                upgrade1To2(sqLiteDatabase);
-                break;
-            default:
-                throw new IllegalStateException("Unknown old version: " + oldVersion);
-        }
-    }
-
-    private void upgrade1To2(SQLiteDatabase database) {
-        database.beginTransaction();
-        try {
-            database.execSQL("ALTER TABLE " + CategoriesTable.TABLE_NAME + " RENAME TO tmp_table");
-            database.execSQL(CategoriesTable.SQL_CREATE_CATEGORIES_TABLE);
-            database.execSQL("INSERT INTO " + CategoriesTable.TABLE_NAME + " (" +
-                        CategoriesTable._ID + ", " +
-                        CategoriesTable.COL_FIRST_ENTRY_DATE + ", " +
-                        CategoriesTable.COL_CATEGORY_NAME + ") " +
-                    "SELECT " +
-                        CategoriesTable._ID + ", " +
-                        "date_created, " +
-                        CategoriesTable.COL_CATEGORY_NAME + " " +
-                        "FROM tmp_table");
-            database.execSQL("DROP TABLE tmp_table");
-            database.setTransactionSuccessful();
-        } finally {
-            database.endTransaction();
-        }
-    }
-
-    @Override
-    public void onDowngrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        if (BuildConfig.DEBUG) {
-            dropTables(sqLiteDatabase);
-            createTables(sqLiteDatabase);
-        }
-    }
-
     public static void dropTables(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EntriesTable.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + CategoriesTable.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + CategoriesView.TABLE_NAME);
     }
 }
