0ab6f97f9dd4dff6c93c39e9a6eb7f33b8c114a8
deanrobertcook <deanrobertcook@gmail.com>
2015-12-05 12:17:09 +0800

    Add Application class to add Timber

---
+++ app/src/main/java/org/theronin/budgettracker/BudgetTrackerApplication.java
@@ -0,0 +1,10 @@
+package org.theronin.budgettracker;
+
+import android.app.Application;
+
+public class BudgetTrackerApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
