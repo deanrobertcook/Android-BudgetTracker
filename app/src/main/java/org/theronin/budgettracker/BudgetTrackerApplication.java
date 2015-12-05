bc7b5871ff748b895ce7cf88da3c76984dcfa086
deanrobertcook <deanrobertcook@gmail.com>
2015-12-05 12:27:09 +0800

    Plant a timber debug tree

--- app/src/main/java/org/theronin/budgettracker/BudgetTrackerApplication.java
+++ app/src/main/java/org/theronin/budgettracker/BudgetTrackerApplication.java
@@ -2,9 +2,14 @@ package org.theronin.budgettracker;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class BudgetTrackerApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 }
