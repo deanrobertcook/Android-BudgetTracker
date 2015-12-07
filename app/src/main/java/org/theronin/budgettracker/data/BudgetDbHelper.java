dcfa804af867c1a9569396d6b547e77ff4498e38
deanrobertcook <deanrobertcook@gmail.com>
2015-12-07 12:29:24 +0800

    Change BudgetDBHelper to Singleton
    This should help prevent any situations where the DB is left un-closed

--- app/src/main/java/org/theronin/budgettracker/data/BudgetDbHelper.java
+++ app/src/main/java/org/theronin/budgettracker/data/BudgetDbHelper.java
@@ -19,9 +19,18 @@ public class BudgetDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "budgettracker.db";
     public static final int DATABASE_VERSION = 1;
 
+    private static BudgetDbHelper instance;
+
     private Context context;
 
-    public BudgetDbHelper(Context context) {
+    public static synchronized BudgetDbHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new BudgetDbHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    private BudgetDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
