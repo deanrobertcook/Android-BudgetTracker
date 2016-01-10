89c7f40f4a1c5bc68b2c907ca1dbad420d7d0be6
deanrobertcook <deanrobertcook@gmail.com>
2016-01-10 13:35:10 +0100

    Provide a function for downloading any exchange rate days for any currencies

--- server/cloud/main.js
+++ server/cloud/main.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 Parse.Cloud.beforeSave("Entry", function(request, response) {
   if (!request.object.get("isDeleted")) {
     request.object.set("isDeleted", false);
@@ -26,68 +28,74 @@ Parse.Cloud.define("sendChangePush", function(request, response) {
     });
 });
 
-//Request expects a currency code and a comma separated list of dates of the form YYYY-MM-DD
+//Request contains a list of currency codes and dates of the form YYYY-MM-DD, both comma separated
 Parse.Cloud.define("exchangeRate", function(request, response) {
     var ExchangeRate = Parse.Object.extend("ExchangeRate");
 
     var dateStrings = request.params.dates.split(",");
+    var codes = request.params.codes.split(",");
 
-    function getDateFromString(dateString) {
-        var startDateParts = startDateString.split("-");
-        var startDate = new Date(startDateParts[0], startDateParts[1], startDateParts[2]);
-    }
-
-    var startDateString = dateStrings[0];
-
-
-    var code = request.params.code;
-
-    for (var dateString in dateStrings) {
-        var dateParts = dateString.split("-");
-        var date = new Date(dateParts[0], dateParts[1], dateParts[2]);
-
-        var query = new Parse.Query(ExchangeRate);
-        query.equalTo("code", code);
-    }
-
-});
-
-
-//A job to be run daily to fetch the exchange rate data
-Parse.Cloud.job("dailyExchangeRate", function(request, status) {
-    Parse.Cloud.httpRequest({
-        url: 'https://openexchangerates.org/api/latest.json',
-        params: {
-            app_id : '30b554493a094428adcdff4abb4d2c7b'
-        }
-    }).then(function(httpResponse) {
-        //TODO check date before continuing (don't bother if we already have today's rates);
-        //TODO also, log this say I can track when the API rates are released each day
-        var responseJson = JSON.parse(httpResponse.text);
-        var rates = responseJson.rates;
-        var exchangeRates = [];
-        for (var code in rates) {
-            if (rates.hasOwnProperty(code)) {
-                var rate = rates[code];
-                var exchangeRate = new Parse.Object("ExchangeRate");
-                exchangeRate.set("currency", code);
-                exchangeRate.set("usdRate", rate);
-
-                exchangeRates.push(exchangeRate);
-            }
+    var query = new Parse.Query(ExchangeRate);
+    query.containedIn("currency", codes);
+    query.containedIn("date", dateStrings);
+    query.find().then(function(results) {
+        var promise = Parse.Promise.as();
+        var datesRetrieved = [];
+        var i = 0;
+        for (i; i < results.length; i++) {
+            var objDate = results[i].get("date");
+            datesRetrieved.push(objDate);
         }
 
-        Parse.Object.saveAll(exchangeRates, {
-            success: function(list) {
-                console.log(list.length + " items saved");
-                status.success("Job completed successfully");
-            },
-            error: function(error) {
-                status.error("Job did not complete successfully");
-            }
+        var missing = dateStrings.filter(function(obj) { return datesRetrieved.indexOf(obj) == -1; });
+        console.log("Missing: " + missing);
+
+        if (missing.length > 0) {
+            console.log("missing " + missing.length + " entries");
+
+            _.each(missing, function(dateString) {
+                console.log("downloading for date : " + dateString);
+                promise = promise.then(function() {
+                    return Parse.Cloud.httpRequest({
+                        url: 'https://openexchangerates.org/api/historical/' + dateString + '.json',
+                        params: {
+                            app_id : '30b554493a094428adcdff4abb4d2c7b'
+                        }
+                    }).then(function(httpResponse) {
+                        var responseJson = JSON.parse(httpResponse.text);
+                        console.log("timestamp: " + responseJson.timestamp);
+                        var rates = responseJson.rates;
+                        console.log(rates);
+                        var exchangeRates = [];
+                        for (var code in rates) {
+                            if (rates.hasOwnProperty(code)) {
+                                var rate = rates[code];
+                                var exchangeRate = new Parse.Object("ExchangeRate");
+                                exchangeRate.set("currency", code);
+                                exchangeRate.set("usdRate", rate);
+                                exchangeRate.set("date", dateString);
+                                exchangeRates.push(exchangeRate);
+                            }
+                        }
+                        console.log("saving " + exchangeRates.length + " exrates");
+                        return Parse.Object.saveAll(exchangeRates);
+                    }, function(httpResponse) {
+                        console.log(httpResponse);
+                        return Parse.Promise.as("Ignore missing dates");
+                    });
+                });
+            });
+        }
+        return promise;
+    }).then(function() {
+        query.find().then(function (results) {
+            response.success(results);
+        }, function(error) {
+            response.error(error);
         });
-    }, function (httpResponse) {
-        console.log("Request failed: " + httpResponse.status);
-        status.error("Job did not complete successfully");
+    }, function(error) {
+        response.error(error);
     });
 });
+
+
