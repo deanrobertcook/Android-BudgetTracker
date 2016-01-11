9efc7bbc5f229cb877e3ad909c9b0a7bd2ad9559
deanrobertcook <deanrobertcook@gmail.com>
2016-01-11 08:56:51 +0100

    Setup basic workinng class to download from my backend instead of openexrate server

--- app/src/debug/java/org/theronin/expensetracker/PlayGroundActivity.java
+++ app/src/debug/java/org/theronin/expensetracker/PlayGroundActivity.java
@@ -4,7 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class PlayGroundActivity extends AppCompatActivity {
+import org.theronin.expensetracker.data.backend.ExchangeRateDownloader;
+import org.theronin.expensetracker.model.Currency;
+import org.theronin.expensetracker.model.ExchangeRate;
+
+import java.util.Arrays;
+import java.util.List;
+
+import timber.log.Timber;
+
+
+public class PlayGroundActivity extends AppCompatActivity implements ExchangeRateDownloader.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +25,31 @@ public class PlayGroundActivity extends AppCompatActivity {
         toolbar.setTitle("PlayGround Activity");
         setSupportActionBar(toolbar);
 
+        ExchangeRateDownloader downloader = new ExchangeRateDownloader(this);
+
+        List<Currency> currencies = Arrays.asList(
+                new Currency("AUD"),
+                new Currency("USD"),
+                new Currency("EUR"),
+                new Currency("WOOP")
+        );
+        List<Long> dates = Arrays.asList(
+                System.currentTimeMillis(),
+                System.currentTimeMillis() - (24 * 60L * 60L * 1000L),
+                System.currentTimeMillis() + 2 * (24 * 60L * 60L * 1000L)
+        );
+
+        downloader.downloadExchangeRates(currencies, dates);
+
+
 //        AutoResizeEditText view = (AutoResizeEditText) findViewById(R.id.test_view);
 //        view.setCurrency(new Currency("AUD", "$", "Australian Dollar"));
+    }
+
+    @Override
+    public void onDownloadComplete(List<ExchangeRate> exchangeRates) {
+        for (ExchangeRate rate : exchangeRates) {
+            Timber.v(rate.toString());
+        }
     }
 }
