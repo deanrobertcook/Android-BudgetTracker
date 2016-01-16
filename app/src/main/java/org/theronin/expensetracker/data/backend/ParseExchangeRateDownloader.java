e50c96e119f647bf72fe5fe1ffb67475ca493664
deanrobertcook <deanrobertcook@gmail.com>
2016-01-16 17:42:48 +0100

    Remove duplicate dates from Parse params on ExchangeRate downloads

--- app/src/main/java/org/theronin/expensetracker/data/backend/ParseExchangeRateDownloader.java
+++ app/src/main/java/org/theronin/expensetracker/data/backend/ParseExchangeRateDownloader.java
@@ -11,10 +11,10 @@ import org.theronin.expensetracker.utils.DateUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-
-import timber.log.Timber;
+import java.util.Set;
 
 public class ParseExchangeRateDownloader implements ExchangeRateDownloader {
 
@@ -36,13 +36,11 @@ public class ParseExchangeRateDownloader implements ExchangeRateDownloader {
 
         //TODO maybe make sure that the ExchangeRates passed in get updated, rather than creating a new list?
         try {
-            Timber.i("Making call to retrieve exchange rates from BE function");
             List<ParseObject> parseObjects = ParseCloud.callFunction("exchangeRate", params);
 
             List<ExchangeRate> rates = new ArrayList<>();
             for (ParseObject parseObject : parseObjects) {
                 ExchangeRate rate = getExchangeRate(parseObject);
-                Timber.v("Downloaded: " + rate.toString());
                 rates.add(rate);
             }
             callback.onDownloadComplete(rates);
@@ -76,13 +74,24 @@ public class ParseExchangeRateDownloader implements ExchangeRateDownloader {
 
     private String createDatesObject(List<ExchangeRate> rates) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < rates.size(); i++) {
-            sb.append(DateUtils.getStorageFormattedDate(rates.get(i).utcDate));
-            if (i < rates.size() - 1) {
+        Set<String> dates = extractUniqueDates(rates);
+        int i = 0;
+        for (String date : dates) {
+            sb.append(date);
+            if (i < dates.size() - 1) {
                 sb.append(",");
             }
+            i++;
         }
         return sb.toString();
+    }
+
+    private Set<String> extractUniqueDates(List<ExchangeRate> rates) {
+        Set<String> dates = new HashSet<>();
+        for (ExchangeRate rate : rates) {
+            dates.add(DateUtils.getStorageFormattedDate(rate.utcDate));
+        }
+        return dates;
     }
 
 }
